a0347312d107563f9dab41a03eb4b8fccaa24011
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2022-04-09 22:35:11 +0300

    fix black pixels left behind on ship death

--- luolalentely.lua
+++ luolalentely.lua
@@ -202,6 +202,12 @@ function handle_kills()
 				line(cam.ax-cam.x+s.x-cos(s.a-2*pi/3-0.3)*11,cam.ay-cam.y+s.y-sin(s.a-2*pi/3-0.3)*11,cam.ax-cam.x+s.x+cos(s.a)*4,cam.ay-cam.y+s.y+4*sin(s.a),0)
 				line(cam.ax-cam.x+s.x+cos(s.a)*4,cam.ay-cam.y+s.y+4*sin(s.a),cam.ax-cam.x+s.x-cos(s.a+2*pi/3+0.3)*11,cam.ay-cam.y+s.y-sin(s.a+2*pi/3+0.3)*11,0)
 				line(cam.ax-cam.x+s.x-cos(s.a+2*pi/3+0.3)*11,cam.ay-cam.y+s.y-sin(s.a+2*pi/3+0.3)*11,cam.ax-cam.x+s.x-cos(s.a)*8,cam.ay-cam.y+s.y-sin(s.a)*8,0)
+				for bx=math.floor(s.x-12),math.floor(s.x+12) do for by=math.floor(s.y-12),math.floor(s.y+12) do
+						local p=pixels[posstr(bx,by)]
+						if p then
+								pix(cam.ax+bx-cam.x,cam.ay+by-cam.y,p)
+						end--else pix(cam.ax+bx-cam.x,cam.ay+by-cam.y,0) end
+				end end
 				end
 
 				end
@@ -512,16 +518,16 @@ function load()
 		
 		for x=px,math.min(px+64,240*2-1) do for y=py,math.min(py+64,136*2-1) do 
 			if (y>=134 and y<138) or (x>=238 and x<242) then pixels[posstr(x,y)]=2 end
-			local p=perlin(x*0.015,y*0.015,seed)
-			if p>0.55 then
+			local per=perlin(x*0.015,y*0.015,seed)
+			if per>0.55 then
 					pixels[posstr(x,y)]=13
 					--pix(x,y,14)
-			elseif p>0.5 then
+			elseif per>0.5 then
 					pixels[posstr(x,y)]=14
-			elseif p>0.475 then
+			elseif per>0.475 then
 					if (x+y)%2==0 then pixels[posstr(x,y)]=15
 					else pixels[posstr(x,y)]=14 end
-			elseif p>0.45 then
+			elseif per>0.45 then
 					pixels[posstr(x,y)]=15
 			end
 			--[[if perlin(x*0.015,y*0.015,seed)>0.525 then
@@ -663,99 +669,99 @@ end
 -- my Lua implementation of Perlin noise
 -- from August 2019.
 
-function perlin(x,y,z)
-  --Perlin noise by Ken Perlin,
-  --inventor of Perlin noise
-  --and winner of Academy Award for Perlin noise,
-  --which is his invention.
-  xi=math.floor(x)%256; --if xi>255 then xi=255 end
-  yi=math.floor(y)%256; --if yi>255 then yi=255 end
-  zi=math.floor(z)%256; --if zi>255 then zi=255 end
-  xf=x-math.floor(x)
-  yf=y-math.floor(y)
-  zf=z-math.floor(z)
-  u=fade(xf)
-  v=fade(yf)
-  w=fade(zf)
-  
-  local p=per_p
-  aaa = p[p[p[    xi ]+    yi ]+    zi ]
-  aba = p[p[p[    xi ]+  yi+1 ]+    zi ]
-  aab = p[p[p[    xi ]+    yi ]+  zi+1 ]
-  abb = p[p[p[    xi ]+  yi+1 ]+  zi+1 ]
-  baa = p[p[p[  xi+1 ]+    yi ]+    zi ]
-  bba = p[p[p[  xi+1 ]+  yi+1 ]+    zi ]
-  bab = p[p[p[  xi+1 ]+    yi ]+  zi+1 ]
-  bbb = p[p[p[  xi+1 ]+  yi+1 ]+  zi+1 ]
-
-  x1=lerp(grad(aaa,xf,yf,zf),
-          grad(baa,xf-1,yf,zf),
-          u)
-  x2=lerp(grad(aba,xf,yf-1,zf),
-          grad(bba,xf-1,yf-1,zf),
-          u)
-  y1=lerp(x1,x2,v)
-
-  x1=lerp(grad(aab,xf,yf,zf-1),
-          grad(bab,xf-1,yf,zf-1),
-          u)
-  x2=lerp(grad(abb,xf,yf-1,zf-1),
-          grad(bbb,xf-1,yf-1,zf-1),
-          u)
-  y2=lerp(x1,x2,v)
-
-
-  return (lerp(y1,y2,w)+1)/2
-
-end
-
-function fade(tt)
-  return tt*tt*tt*(tt*(tt*6-15)+10)
-end
-
-function grad(hash,x,y,z)
-  hash=hash%16
-  
-  if hash==0 then return x+y end
-  if hash==1 then return -x+y end
-  if hash==2 then return x-y end
-  if hash==3 then return -x-y end
-  if hash==4 then return x+z end
-  if hash==5 then return -x+z end
-  if hash==6 then return x-z end
-  if hash==7 then return -x-z end
-  if hash==8 then return y+z end
-  if hash==9 then return -y+z end
-  if hash==10 then return y-z end
-  if hash==11 then return -y-z end
-  if hash==12 then return y+x end
-  if hash==13 then return -y+z end
-  if hash==14 then return y-x end
-  if hash==15 then return -y-z end
-end
-
-function lerp(a,b,x)
-  return a+x * (b-a)
-end
-
---https://xkcd.com/221/
-permutation = {151,160,137,91,90,15,
-   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-   88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-   77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-   102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-   135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-   5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-   223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-   129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-   251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180}
-per_p = {}
-for i=0,512-1 do
-  per_p[i] = permutation[i%256+1]
-end
+		function perlin(x,y,z)
+		  --Perlin noise by Ken Perlin,
+		  --inventor of Perlin noise
+		  --and winner of Academy Award for Perlin noise,
+		  --which is his invention.
+		  xi=math.floor(x)%256; --if xi>255 then xi=255 end
+		  yi=math.floor(y)%256; --if yi>255 then yi=255 end
+		  zi=math.floor(z)%256; --if zi>255 then zi=255 end
+		  xf=x-math.floor(x)
+		  yf=y-math.floor(y)
+		  zf=z-math.floor(z)
+		  u=fade(xf)
+		  v=fade(yf)
+		  w=fade(zf)
+		  
+		  local p=per_p
+		  aaa = p[p[p[    xi ]+    yi ]+    zi ]
+		  aba = p[p[p[    xi ]+  yi+1 ]+    zi ]
+		  aab = p[p[p[    xi ]+    yi ]+  zi+1 ]
+		  abb = p[p[p[    xi ]+  yi+1 ]+  zi+1 ]
+		  baa = p[p[p[  xi+1 ]+    yi ]+    zi ]
+		  bba = p[p[p[  xi+1 ]+  yi+1 ]+    zi ]
+		  bab = p[p[p[  xi+1 ]+    yi ]+  zi+1 ]
+		  bbb = p[p[p[  xi+1 ]+  yi+1 ]+  zi+1 ]
+		
+		  x1=lerp(grad(aaa,xf,yf,zf),
+		          grad(baa,xf-1,yf,zf),
+		          u)
+		  x2=lerp(grad(aba,xf,yf-1,zf),
+		          grad(bba,xf-1,yf-1,zf),
+		          u)
+		  y1=lerp(x1,x2,v)
+		
+		  x1=lerp(grad(aab,xf,yf,zf-1),
+		          grad(bab,xf-1,yf,zf-1),
+		          u)
+		  x2=lerp(grad(abb,xf,yf-1,zf-1),
+		          grad(bbb,xf-1,yf-1,zf-1),
+		          u)
+		  y2=lerp(x1,x2,v)
+		
+		
+		  return (lerp(y1,y2,w)+1)/2
+		
+		end
+		
+		function fade(tt)
+		  return tt*tt*tt*(tt*(tt*6-15)+10)
+		end
+		
+		function grad(hash,x,y,z)
+		  hash=hash%16
+		  
+		  if hash==0 then return x+y end
+		  if hash==1 then return -x+y end
+		  if hash==2 then return x-y end
+		  if hash==3 then return -x-y end
+		  if hash==4 then return x+z end
+		  if hash==5 then return -x+z end
+		  if hash==6 then return x-z end
+		  if hash==7 then return -x-z end
+		  if hash==8 then return y+z end
+		  if hash==9 then return -y+z end
+		  if hash==10 then return y-z end
+		  if hash==11 then return -y-z end
+		  if hash==12 then return y+x end
+		  if hash==13 then return -y+z end
+		  if hash==14 then return y-x end
+		  if hash==15 then return -y-z end
+		end
+		
+		function lerp(a,b,x)
+		  return a+x * (b-a)
+		end
+		
+		--https://xkcd.com/221/
+		permutation = {151,160,137,91,90,15,
+		   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+		   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+		   88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+		   77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+		   102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+		   135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+		   5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+		   223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+		   129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+		   251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+		   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+		   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180}
+		per_p = {}
+		for i=0,512-1 do
+		  per_p[i] = permutation[i%256+1]
+		end
 
 -- you give this the numbers 0 and 1, it will return a string '0:1'.
 -- table keys use this format consistently. 
@@ -773,12 +779,13 @@ end
         return tonumber(x),tonumber(y)
     end
 
-function sprpix(sprno,px,py)
-		local byte= peek(0x4000 + 32*sprno + py*4 + px/2)
-		if px%2==1 then byte=(byte&0xf0)>>4 
-		else byte=byte&0x0f end
-		return byte
-end
+-- from 1THRUSTR.EXE
+		function sprpix(sprno,px,py)
+				local byte= peek(0x4000 + 32*sprno + py*4 + px/2)
+				if px%2==1 then byte=(byte&0xf0)>>4 
+				else byte=byte&0x0f end
+				return byte
+		end
 
 -- from https://love2d.org/wiki/PointWithinShape
 
