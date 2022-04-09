603cfde3e887de8163b67fd005f26c3cb12df00b
verysoftwares <54413321+verysoftwares@users.noreply.github.com>
2022-04-09 17:54:17 +0300

    further UI tweaks

--- luolalentely.lua
+++ luolalentely.lua
@@ -542,9 +542,10 @@ cycle={i=1}
 function mainmenu()
 		cls(0)
 		for i=1,4 do
-		print('LUALA',180-40-40+16-(i*4),40-20+16-(i*4),i,false,4,false)
+		print('LUALA',180-40-40+16-(i*4),40-20+16-(i*4)-8,i,false,4,false)
 		end
-		print('Version 1c',180-40-40+40+2,40+4*6-20+10,13,false,1,true)
+		--print('Version 1c',180-40-40+40+2,40+4*6-20+10,13,false,1,true)
+		print('Version 1c',2,136-8,13,false,1,true)
 
 		if btnp(0) or btnp(2) then cycle.i=cycle.i-1; if cycle.i<1 then cycle.i=4 end end
 		if btnp(1) or btnp(3) then cycle.i=cycle.i+1; if cycle.i>4 then cycle.i=1 end end
